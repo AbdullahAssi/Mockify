de926c9cd9fc568029c8ba43d622ae2789cb9ed7
Muhammad Abdullah <113567773+AbdullahAssi@users.noreply.github.com>
2024-11-01 15:38:28 +0500

    day 3 after an year

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -1,4 +1,4 @@
 export default async function Page(){
     await new Promise(resolve => setTimeout(resolve, 2000))
-    return <h1>This is an About Page</h1>
+    return <h1>This is an About Page for next js learning</h1>
 }
